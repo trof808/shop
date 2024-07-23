53603d622477f60867fdce2c5e48b7d91dbb0bd6
Dmitry Dolgushin <razor9714@gmail.com>
2024-07-23 17:59:15 +0500

    added generics in apiService

--- src/shared/api/apiService.ts
+++ src/shared/api/apiService.ts
@@ -1,7 +1,7 @@
 import { host } from '@/shared/constants';
 import axios from 'axios';
 
-export abstract class ApiService {
+export abstract class ApiService<T> {
 	private baseUrl: string;
 
 	constructor(baseUrl: string) {
@@ -14,7 +14,7 @@ export abstract class ApiService {
 	}: {
 		path?: string;
 		params?: Record<string, any>;
-	} = {}): Promise<any> {
+	} = {}): Promise<T> {
 		return await axios({
 			method: 'GET',
 			url: `${this.baseUrl}${path}`,
@@ -25,7 +25,7 @@ export abstract class ApiService {
 	public async create(
 		path: string = '',
 		body: Record<string, any>
-	): Promise<any> {
+	): Promise<T> {
 		return await axios({
 			method: 'POST',
 			data: body,
@@ -36,7 +36,7 @@ export abstract class ApiService {
 	public async update(
 		path: string = '',
 		payload: Record<string, any>
-	): Promise<any> {
+	): Promise<T> {
 		const bodyParams = JSON.stringify(payload);
 		return await axios({
 			method: 'PUT',
@@ -45,7 +45,7 @@ export abstract class ApiService {
 		});
 	}
 
-	public async delete(path: string = ''): Promise<any> {
+	public async delete(path: string = ''): Promise<T> {
 		return await axios<ResponseType>({
 			method: 'DELETE',
 			url: `${this.baseUrl}/${path}`,
